2e5d42523618a818369c74739aad35782837d43e
infinitey <infinitey@live.com>
2017-07-18 10:45:42 +0800

    fix bugs for actions

--- app/Home/home_actions.js
+++ app/Home/home_actions.js
@@ -2,7 +2,7 @@ export const RETRIEVE_DATA_PENDING = 'RETRIEVE_DATA_PENDING'
 export const RETRIEVE_DATA_SUCCESS = 'RETRIEVE_DATA_SUCCESS'
 export const RETRIEVE_DATA_FAILURE = 'RETRIEVE_DATA_FAILURE'
 
-export const retrieveDataPending () => {
+export const retrieveDataPending = () => {
   return {
     type: RETRIEVE_DATA_PENDING,
     isDataPending: true,
@@ -10,15 +10,15 @@ export const retrieveDataPending () => {
 }
 
 
-export const retrieveDataSuccess ( dataObj ) => {
+export const retrieveDataSuccess = ( dataObj ) => {
   return {
     type: RETRIEVE_DATA_SUCCESS,
-    isDataPending: false, //testing source control
+    isDataPending: false, 
     dataObj: dataObj
   }
 }
 
-export const retrieveDataFailure (error) => {
+export const retrieveDataFailure = ( error ) => {
   return {
     type: RETRIEVE_DATA_FAILURE,
     isDataPending: false,
